64e247cc59348df7d57d565ff57418bbc4937da9
Jaime J Cobo <jaimecobo@outlook.com>
2021-07-25 21:39:30 -0600

    React tic-tac-toe // TypeScript // Firebase:Angular

---
+++ firebase-authentication/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAAL41x0UUGmPX3A2WL0E-kx_ORl9A2aqs",
+    authDomain: "fir-authentication-2f4f3.firebaseapp.com",
+    projectId: "fir-authentication-2f4f3",
+    storageBucket: "fir-authentication-2f4f3.appspot.com",
+    messagingSenderId: "659800065754",
+    appId: "1:659800065754:web:4a39b30eda82b20f7647e3",
+    measurementId: "G-G5J8KGFF42"
+  },
+};
---
+++ firebase-authentication/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'YOUR API KEY',
+    authDomain: 'project.firebaseapp.com',
+    databaseURL: 'https://project.firebaseio.com',
+    projectId: 'project',
+    storageBucket: 'project.appspot.com',
+    messagingSenderId: 'YOUR MESSAGINGSENDERID',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
