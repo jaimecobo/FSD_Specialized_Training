9a83c46dec4ff79bd337dfc95ab98348c488704f
Jaime J Cobo <jaimecobo@outlook.com>
2021-08-18 17:57:35 -0600

    MainController SpringBoot Subscriber form that submits to H2

---
+++ 43 Spring Boot JPA Annotations/Gradle/SubscriberList/src/main/java/com/example/SubscriberList/repository/SubscriberRepository.java
@@ -0,0 +1,9 @@
+package com.example.SubscriberList.repository;
+
+import com.example.SubscriberList.model.Subscriber;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface repository extends CrudRepository<Subscriber, Long> {
+
+}
